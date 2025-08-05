05c5d597394ec851e0cbdc8458e8ef3b288b7a1d
PibeFlama <142538580+PibeFlama@users.noreply.github.com>
2025-08-05 16:51:25 +0000

    fgfgfg

--- unidad_04/hola_00.py
+++ unidad_04/hola_00.py
@@ -1,2 +1,10 @@
-x = int(input("¿Cuál es x? "))
+while True:
+    try: 
+        x = int(input("¿Cuál es x? "))
+    except ValueError:
+        print("x no es entero")
+    else: 
+        break
+
 print(f"x es {x}")
+
